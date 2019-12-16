8db34a0e03e022540bcaef06f0069cf5f9607bfa
StavZDev <randy10220@gmail.com>
2019-12-16 17:16:55 +0300

    UHC Added

--- structures/MGs.js
+++ structures/MGs.js
@@ -1,7 +1,9 @@
 const SkyWars = require('./MiniGames/SkyWars');
 const BedWars = require('./MiniGames/BedWars');
+const UHC = require('./MiniGames/UHC')
 
 module.exports = {
     SkyWars: SkyWars,
-    BedWars: BedWars
+    BedWars: BedWars,
+    UHC: UHC
 }
---
+++ structures/MiniGames/UHC.js
@@ -0,0 +1,17 @@
+class UHC {
+    constructor(data) {
+        //General
+        this.coins = data.coins;
+
+        //Stats
+        this.deaths = data.deaths;
+        this.kills = data.kills
+        this.uhc = {
+            score:
+        }
+        this.speed = {
+
+        }
+    }
+}
+module.exports = UHC;
