6d3438e82898156ebc0a2904e898da8724cee985
StavZDev <randy10220@gmail.com>
2019-12-17 18:57:55 +0300

    Added UHC/SpeedUHC

--- structures/MGs.js
+++ structures/MGs.js
@@ -1,9 +1,11 @@
 const SkyWars = require('./MiniGames/SkyWars');
 const BedWars = require('./MiniGames/BedWars');
 const UHC = require('./MiniGames/UHC')
+const SpeedUHC = require('./MiniGames/SpeedUHC')
 
 module.exports = {
     SkyWars: SkyWars,
     BedWars: BedWars,
-    UHC: UHC
+    UHC: UHC,
+    SpeedUHC: SpeedUHC
 }
