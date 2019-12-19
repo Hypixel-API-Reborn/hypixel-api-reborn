6cd94ee08243d40c00a91fafd1cf27e4f32d2d64
StavZDev <randy10220@gmail.com>
2019-12-19 14:35:38 +0300

    Added api key validation

--- Client.js
+++ Client.js
@@ -1,9 +1,9 @@
 const fetch = require('node-fetch');
 
-const BASE_URL = 'https://api.hypixel.net'
+const BASE_URL = 'https://api.hypixel.net';
 
-const Player = require('./structures/Player')
-const getUuid = require('./utils/getUuid')
+const Player = require('./structures/Player');
+const getUuid = require('./utils/getUuid');
 
 class Client {
     /**
@@ -16,12 +16,29 @@ class Client {
         this.compacted = compacted;
     }
 
+    /**
+     * @description Checks key validity
+     * @async
+     * @returns {Boolean}
+     * @param {String} key 
+     */
+    async validateApiKey(key) {
+        let check = await fetch(BASE_URL + '/key' + `?key=${this.key}`).then(r => r.json());
+        if (check.success == false && check.cause == 'Invalid API key!') {
+            return false;
+        } else {
+            return true;
+        };
+    }
 
     /**
+     * @description Checks input on UUID or on player name
+     * @async
      * @returns {Boolean} 
+     * 
      * @param {String} uuid - valid Minecraft Player UUID
      */
-    isUUID(uuid) {
+    async isUUID(uuid) {
         let f = new RegExp(`[0-9a-fA-F]{8}[0-9a-fA-F]{4}[0-9a-fA-F]{4}[0-9a-fA-F]{4}[0-9a-fA-F]{12}`);
         let s = new RegExp(`[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}`);
         let test = f.test(uuid);
@@ -32,39 +49,48 @@ class Client {
 
     /**
      * @async
-     * @returns {Promise} 
+     * @returns {Promise} Hypixel player Object or Error
+     * 
      * @param {String} uuid 
      */
-    async getPlayer (uuid) {
-        return new Promise(async (resolve, reject) => {
-            if(this.isUUID(uuid) == false) {
-                let uid = await getUuid(uuid)
-                if(uid == 'Player does not exist') {
-                    return reject({
-                        player: null,
-                        error: 'Player does not exist'
-                    })
-                }
-                uuid = uid;
-            }
-            fetch(BASE_URL + `/player` + `?key=${this.key}` + `&uuid=${uuid}`).then(r => r.json()).then(res => {
-                if(res.success == false && res.cause == 'Invalid API key!') {
-                    return reject({error: res.cause})
-                }
-                if(!res.player || res.cause == 'Malformed UUID!') {
-                    return reject({
-                        player: null,
-                        error: 'Player does not exist'
-                    })
-                }
-                
-                if(this.compacted == true) {
-                    return resolve(new Player(res))
-                } else {
-                    return resolve(res)
-                }
-            })
-        })
+    async getPlayer(uuid) {
+        let checkUUID = await this.isUUID(uuid);
+        if (checkUUID == false) {
+            let uid = await getUuid(uuid)
+            if (uid == 'Player does not exist') {
+                return {
+                    success: false,
+                    error: 'Player does not exist'
+                };
+            };
+            uuid = uid;
+        };
+        let validKey = await this.validateApiKey(this.key);
+        console.log(validKey)
+        if (validKey == false) {
+            return {
+                success: false,
+                error: 'Invalid API key!'
+            };
+        };
+        let res = await fetch(BASE_URL + `/player` + `?key=${this.key}` + `&uuid=${uuid}`).then(r => r.json());
+        if (!res.player || res.cause == 'Malformed UUID!') {
+            return {
+                success: false,
+                error: 'Player does not exist'
+            };
+        };
+
+        if (this.compacted == true) {
+            return {
+                success: true,
+                player: await new Player(res)
+            };
+        };
+        return {
+            success: true,
+            player: res
+        };
     }
 }
 
