f9a440653809751b5a23ff508fdf4ea4db75dff1
StavZ <randy10220@gmail.com>
2020-01-18 21:47:09 +0300

    Create Friend.js

---
+++ structures/Friend.js
@@ -0,0 +1,11 @@
+class Friend {
+    constructor(data) {
+
+        this.sender = data.uuidSender;
+        this.receiver = data.uuidReceiver;
+        this.friendSince = data.started;
+        
+    }
+}
+
+module.exports = Friend;
