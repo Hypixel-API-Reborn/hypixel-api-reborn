318fb375c989a844f85f586b4ca5a89f8f946fbc
StavZ <randy10220@gmail.com>
2020-01-18 21:45:48 +0300

    getWatchdogStats method

---
+++ structures/Watchdog/Stats.js
@@ -0,0 +1,11 @@
+class WatchdogStats {
+    constructor (data) {
+        this.ByWatchdogTotal = data.watchdog_total || 0;
+        this.ByWatchDogLastMinute = data.watchdog_lastMinute || 0;
+        this.ByWatchdogRollingDay = data.watchdog_rollingDaily || 0;
+
+        this.ByStaffTotal = data.staff_total || 0;
+        this.ByStaffRollingDay = data.staff_rollingDaily || 0;
+    }
+}
+module.exports = WatchdogStats;
