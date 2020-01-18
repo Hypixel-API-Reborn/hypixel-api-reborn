541cd7138d387a206fdd70d8daeaf9e339a1881e
StavZ <randy10220@gmail.com>
2020-01-18 20:55:57 +0300

    MG-Import

---
+++ structures/MiniGames/Import.js
@@ -0,0 +1,17 @@
+const SkyWars = require('./SkyWars');
+const BedWars = require('./BedWars');
+const UHC = require('./UHC')
+const SpeedUHC = require('./SpeedUHC')
+const MurderMystery = require('./MurderMystery');
+const Duels = require('./Duels')
+const CrazyWalls = require('./CrazyWalls')
+
+module.exports = {
+    SkyWars: SkyWars,
+    BedWars: BedWars,
+    UHC: UHC,
+    SpeedUHC: SpeedUHC,
+    MurderMystery: MurderMystery,
+    Duels: Duels,
+    CrazyWalls: CrazyWalls
+}
