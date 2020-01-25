34b998ad7e050b3bc0e0c89ffc774657df121646
StavZ <randy10220@gmail.com>
2020-01-25 16:47:52 +0300

    winstreak/wins are undefined FIX

--- structures/MiniGames/SpeedUHC.js
+++ structures/MiniGames/SpeedUHC.js
@@ -1,12 +1,12 @@
 class SpeedUHC {
     constructor(data) {
-        this.coins = data.coins;
-        this.wins = data.wins;
-        this.kills = data.kills;
-        this.playedGames = data.games;
-        this.winstreak = data.winstreak;
-        this.deaths = data.deaths;
-        this.losses = data.losses
+        this.coins = data.coins || 0;
+        this.wins = data.wins || 0;
+        this.kills = data.kills || 0;
+        this.playedGames = data.games || 0;
+        this.winstreak = data.win_streak || 0;
+        this.deaths = data.deaths || 0;
+        this.losses = data.losses || 0;
         this.KDRatio = Math.round(((this.kills || 0) / (this.deaths || 0)) * 100) / 100;
         this.WLRatio = Math.round(((this.wins || 0) / (this.losses || 0)) * 100) / 100;
     }
