f968af1fdfadc34bbb882d905b2a1371ef06f353
StavZ <randy10220@gmail.com>
2020-01-26 14:46:28 +0300

    Update Guild.js

--- structures/Guild/Guild.js
+++ structures/Guild/Guild.js
@@ -5,30 +5,30 @@ const Games = require('../../utils/MiniGames')
 
 class Guild {
     constructor(data) {
-        this.id = data._id;
-        this.name = data.name;
-        this.description = data.description ? data.description : null;
+        this.id = data['_id'];
+        this.name = data['name'];
+        this.description = data['description'] ? data['description'] : null;
 
-        this.coins = data.coinsEver;
+        this.coins = data['coinsEver'];
 
-        this.createdAt = data.created;
-        this.joinable = data.joinable ? data.joinable : false;
-        this.publiclyListed = data.publiclyListed;
+        this.createdAt = data['created'];
+        this.joinable = data['joinable'] ? data['joinable'] : false;
+        this.publiclyListed = data['publiclyListed'];
 
-        this.members = data.members ? data.members.map(m => new GuildMember(m)) : null;
-        this.ranks = data.ranks ? data.ranks.map(r => new GuildRank(r)) : null;
+        this.members = data['members'] ? data['members'].map(m => new GuildMember(m)) : null;
+        this.ranks = data['ranks'] ? data['ranks'].map(r => new GuildRank(r)) : null;
 
         this.tag = {
-            string: data.tag ? data.tag : null,
-            color: data.tagColor ? Color.ColorString[data.tagColor] : null,
-            hexColor: data.tagColor ? Color.ColorHex[data.tagColor] : null
+            string: data['tag'] ? data['tag'] : null,
+            color: data['tagColor'] ? Color.ColorString[data['tagColor']] : null,
+            hexColor: data['tagColor'] ? Color.ColorHex[data['tagColor']] : null
         }
 
         this.legacyRank = parseInt(data.legacyRanking)+1;
         this.achievements = {
-            winners: data.achievements ? data.achievements.WINNERS : 0,
-            experienceKings: data.achievements ? data.achievements.EXPERIENCE_KINGS : 0,
-            onlinePlayers: data.achievements ? data.achievements.ONLINE_PLAYERS : 0
+            winners: data['achievements'] ? data['achievements'].WINNERS : 0,
+            experienceKings: data['achievements'] ? data['achievements'].EXPERIENCE_KINGS : 0,
+            onlinePlayers: data['achievements'] ? data['achievements'].ONLINE_PLAYERS : 0
         }
         this.preferredGames = data.preferredGames ? data.preferredGames.map(g => Games[g]) : 0;
 
