a564604d9633878473f124a6e78401269b9c9de2
linearaccelerator <64271566+linearaccelerator@users.noreply.github.com>
2020-07-10 21:07:49 +0200

    Update Guild.js
    added a feature to get a Map of guild members and guild member UUID (might not work)

--- src/structures/Guild/Guild.js
+++ src/structures/Guild/Guild.js
@@ -32,6 +32,11 @@ class Guild {
   get members () {
     return this._data.members ? this._data.members.map(m => new GuildMember(m)) : null;
   }
+  /**
+  * @returns {Map<GuildMember, string}
+  */
+  get memberUUIDMap () {
+    return this._data.members ? new Map(this._data.members.map(m => [new GuildMember(m), new GuildMember(m).uuid])) : null;
 
   /**
    * @returns {Array<GuildRank>}
