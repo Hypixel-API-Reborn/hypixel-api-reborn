f75724f17e6a3410423c7e5eaba5b9514239122f
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-10-31 20:16:14 +0100

    Update objectToArray.js
    Optimization with Array.from

--- src/utils/objectToArray.js
+++ src/utils/objectToArray.js
@@ -1,9 +1,3 @@
 module.exports = (object) => {
-  const array = [];
-
-  const objectLength = Object.keys(object).length;
-  for (let i = 0; i < objectLength; i++) {
-    array.push(Object.keys(object)[i]);
-  }
-  return array;
+  return Array.from(Object.keys(object))
 };
