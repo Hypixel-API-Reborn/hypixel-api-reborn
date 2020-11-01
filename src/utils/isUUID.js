25095a37441f954f09de082aa9c95a354fedf404
StavZDev <36641973+StavZ@users.noreply.github.com>
2020-11-01 13:18:17 +0500

    Update isUUID.js
    No longer used

--- src/utils/isUUID.js
+++ src/utils/isUUID.js
@@ -1,4 +1,5 @@
 module.exports = (uuid) => {
-  const regexp = /[0-9a-f]{8}(-)?[0-9a-f]{4}(-)?[0-9a-f]{4}(-)?[0-9a-f]{4}(-)?[0-9a-f]{12}/gim;
+  const regexp = /[0-9a-f]{8}[0-9a-f]{4}[0-9a-f]{4}[0-9a-f]{4}[0-9a-f]{12}/gim;
+  uuid = uuid.replace(/-/g, '');
   return regexp.test(uuid);
 };
