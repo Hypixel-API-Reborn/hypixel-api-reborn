ed50186b7b1e2df7a7c68f9f8eaaa093075a8a1b
StavZDev <36641973+StavZ@users.noreply.github.com>
2020-11-01 17:35:30 +0500

    bruh

--- src/utils/toUuid.js
+++ src/utils/toUuid.js
@@ -2,15 +2,11 @@ const fetch = require('node-fetch');
 const isUUID = require('./isUUID.js');
 const Errors = require('../Errors');
 module.exports = async (input) => {
-  try {
-    if (isUUID(input)) return input.replace(/-/g, '');
-    const res = await fetch(`https://api.mojang.com/users/profiles/minecraft/${input}`);
-    if (res.status && res.status === 204) {
-      throw new Error(Errors.PLAYER_DOES_NOT_EXIST);
-    }
-    const parsedRes = await res.json();
-    return parsedRes.id;
-  } catch (e) {
-    throw new Error(Errors.PLAYER_DOES_NOT_EXIST);
+  if (isUUID(input)) return input.replace(/-/g, '');
+  const res = await fetch(`https://api.mojang.com/users/profiles/minecraft/${input}`);
+  const parsedRes = await res.json();
+  if (parsedRes.error) {
+    throw new Error(Errors.MALFORMED_UUID);
   }
+  return parsedRes.id;
 };
