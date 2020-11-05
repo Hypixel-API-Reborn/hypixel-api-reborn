64c8b6c5ca8b2d46dd8b4daeb9333f9b255b1fba
StavZDev <36641973+StavZ@users.noreply.github.com>
2020-11-05 14:35:04 +0500

    Update toUuid.js

--- src/utils/toUuid.js
+++ src/utils/toUuid.js
@@ -4,6 +4,7 @@ const Errors = require('../Errors');
 
 module.exports = async (input) => {
   try {
+    if (!input) throw new Error(Errors.NO_NICKNAME_UUID);
     if (typeof input !== 'string') throw new Error(Errors.UUID_NICKNAME_MUST_BE_A_STRING);
     if (isUUID(input)) return input.replace(/-/g, '');
     const res = await fetch(`https://api.mojang.com/users/profiles/minecraft/${input}`);
