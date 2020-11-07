7256f6188cde8c47a01ce214d28432dd73b99c07
ChiefChippy2 <chiefchippy2@gmail.com>
2020-11-07 15:46:49 +0100

    flat polyfill

--- src/API/index.js
+++ src/API/index.js
@@ -1,4 +1,24 @@
+/* eslint-disable no-extend-native */
 const fs = require('fs');
+if (!Array.prototype.flat) {
+  Object.defineProperty(Array.prototype, 'flat', {
+    configurable: true,
+    value: function flat () {
+      var depth = isNaN(arguments[0]) ? 1 : Number(arguments[0]);
+
+      return depth ? Array.prototype.reduce.call(this, function (acc, cur) {
+        if (Array.isArray(cur)) {
+          acc.push.apply(acc, flat.call(cur, depth - 1));
+        } else {
+          acc.push(cur);
+        }
+
+        return acc;
+      }, []) : Array.prototype.slice.call(this);
+    },
+    writable: true
+  });
+}
 // eslint-disable-next-line no-path-concat
 const curDir = __dirname + '/';
 module.exports = Array.from(fs.readdirSync(curDir, { withFileTypes: true }))
