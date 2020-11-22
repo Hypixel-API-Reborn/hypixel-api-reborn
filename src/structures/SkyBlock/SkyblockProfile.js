328c1e6d1e58dfc803bdeafe729b0acfe718350c
StavZDev <36641973+StavZ@users.noreply.github.com>
2020-11-22 11:35:35 +0500

    Update SkyblockProfile.js

--- src/structures/SkyBlock/SkyblockProfile.js
+++ src/structures/SkyBlock/SkyblockProfile.js
@@ -3,7 +3,7 @@ class SkyblockProfile {
   constructor (data) {
     this.profileId = data.profile_id;
     this.profileName = data.profile_name;
-    this.members = edit(data.members).map(m => new Member(m));
+    this.members = edit(data.members, data.profile_name).map(m => new Member(m));
     this.me = this.members.find(x => x.uuid === data.me);
   }
 }
