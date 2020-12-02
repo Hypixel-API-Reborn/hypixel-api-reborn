14b24f01a170096b065209f444b468191c16286a
ChiefChippy2 <chiefchippy2@gmail.com>
2020-12-02 17:10:19 +0100

    Patches + Features
    FIXES :
    Fixed typo in typings (qeury)
    Fixed impossibility to fetch page 0 of skyblock AH
    Removed hardcode for returning headers, now it is always returned in the property _headers
    
    Features
    
    Added AuctionInfo -> Additional info about the page, total page, total auctions, last updated ( ts and date ), age

--- src/structures/KeyInfo.js
+++ src/structures/KeyInfo.js
@@ -1,11 +1,12 @@
 class KeyInfo {
   constructor (data) {
-    this.key = data[0].key;
-    this.owner = data[0].owner;
-    this.limitPerMinute = data[0].limit || 0;
-    this.requestsInPastMin = data[0].queriesInPastMin || 0;
-    this.totalRequests = data[0].totalQueries || 0;
-    this.resetsAfter = parseInt(data[1].get('ratelimit-reset'));
+    const record = data.record;
+    this.key = record.key;
+    this.owner = record.owner;
+    this.limitPerMinute = record.limit || 0;
+    this.requestsInPastMin = record.queriesInPastMin || 0;
+    this.totalRequests = record.totalQueries || 0;
+    this.resetsAfter = parseInt(data._headers.get('ratelimit-reset'));
   }
 }
 module.exports = KeyInfo;
