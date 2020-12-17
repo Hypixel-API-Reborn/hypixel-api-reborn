c5ccb287d9bb2c7d96a5ff9298080a487a2815e7
ChiefChippy2 <chiefchippy2@gmail.com>
2020-12-17 21:37:11 +0100

    New feature : getAPIStatus.

---
+++ src/structures/APIIncident.js
@@ -0,0 +1,16 @@
+class APIIncident {
+  constructor (data) {
+    this.link = data.link || null;
+    this.start = new Date(data.pubDate) || null;
+    this.startFormatted = data.pubDate || null;
+    this.startTimestamp = new Date(data.pubDate).getTime() || null;
+    this.author = data.creator || null;
+    this.HTMLContent = data.content || null;
+    this.snippet = data.contentSnippet || null;
+    this.TextContent = (data.content || '').replace(/<[^>]+>/g, '') || null;
+    this.guid = data.guid || null;
+    this.categories = data.categories || null;
+  }
+}
+
+module.exports = APIIncident;
---
+++ src/structures/APIStatus.js
@@ -0,0 +1,11 @@
+const APIIncident = require('./APIIncident');
+class APIStatus {
+  constructor (data) {
+    this.sourceUrl = data.feedUrl || null;
+    this.title = data.title || null;
+    this.desceription = data.description || null;
+    this.incidents = data.items.map(x => new APIIncident(x));
+  }
+}
+
+module.exports = APIStatus;
