9a4c112582435cd0359ea45c099dd256c9af6357
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2020-12-20 15:36:34 +0100

    Update APIStatus.js
    descerption

--- src/structures/APIStatus.js
+++ src/structures/APIStatus.js
@@ -3,7 +3,7 @@ class APIStatus {
   constructor (data) {
     this.sourceUrl = data.feedUrl || null;
     this.title = data.title || null;
-    this.desceription = data.description || null;
+    this.description = data.description || null;
     this.incidents = data.items.map(x => new APIIncident(x));
   }
 }
