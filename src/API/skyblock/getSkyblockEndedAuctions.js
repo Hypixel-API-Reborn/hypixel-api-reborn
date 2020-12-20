00d249472c042839bc63df506ea92090c3ce13dc
ChiefChippy2 <chiefchippy2@gmail.com>
2020-12-20 14:08:52 +0100

    +Generalized info structure

--- src/API/skyblock/getSkyblockEndedAuctions.js
+++ src/API/skyblock/getSkyblockEndedAuctions.js
@@ -1,9 +1,9 @@
 const PartialAuction = require('../../structures/SkyBlock/Auctions/PartialAuction');
+const AuctionInfo = require('../../structures/SkyBlock/Auctions/AuctionInfo');
 module.exports = async function (includeItemBytes = false) {
   const res = await this._makeRequest('/skyblock/auctions_ended');
   return {
-    lastUpdatedTimestamp: res.lastUpdated,
-    lastUpdatedAt: new Date(res.lastUpdated),
+    info: new AuctionInfo({ ...res, totalAuctions: res.auctions.length, totalPages: 1 }),
     auctions: res.auctions.length ? res.auctions.map(a => new PartialAuction(a, includeItemBytes)) : []
   };
 };
--- src/structures/SkyBlock/Auctions/AuctionInfo.js
+++ src/structures/SkyBlock/Auctions/AuctionInfo.js
@@ -1,9 +1,9 @@
 module.exports = class AuctionInfo {
   constructor (data) {
-    this.page = parseInt(data.page);
-    this.totalPages = parseInt(data.totalPages);
-    this.totalAuctions = parseInt(data.totalAuctions);
-    this.lastUpdatedTimestamp = parseInt(data.lastUpdated);
+    this.page = parseInt(data.page) || 0;
+    this.totalPages = parseInt(data.totalPages) || 1;
+    this.totalAuctions = parseInt(data.totalAuctions) || NaN;
+    this.lastUpdatedTimestamp = parseInt(data.lastUpdated) || 0;
     this.lastUpdatedAt = new Date(data.lastUpdated);
     this.age = parseInt(data._headers.get('age')) || 0;
   }
