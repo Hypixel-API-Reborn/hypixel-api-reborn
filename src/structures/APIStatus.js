c366e7fef20e67fef475fe1abb78e46a3abd193b
StavZDev <36641973+StavZ@users.noreply.github.com>
2020-12-21 14:31:35 +0500

    j s d o c

--- src/structures/APIStatus.js
+++ src/structures/APIStatus.js
@@ -1,9 +1,25 @@
 const APIIncident = require('./APIIncident');
 class APIStatus {
   constructor (data) {
+    /**
+     * Source url
+     * @type {string|null}
+     */
     this.sourceUrl = data.feedUrl || null;
+    /**
+     * Title
+     * @type {string|null}
+     */
     this.title = data.title || null;
+    /**
+     * Description
+     * @type {string|null}
+     */
     this.description = data.description || null;
+    /**
+     * API incident
+     * @type {APIIncident[]}
+     */
     this.incidents = data.items.map(x => new APIIncident(x));
   }
 }
