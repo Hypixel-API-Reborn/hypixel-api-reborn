bff1a6a5c63152e58f62aaea00a69ffcdf583d32
ChiefChippy2 <chiefchippy2@gmail.com>
2021-03-08 17:00:44 +0100

    ranked stats

--- src/structures/MiniGames/SkyWars.js
+++ src/structures/MiniGames/SkyWars.js
@@ -192,7 +192,8 @@ class SkyWars {
       losses: data.losses_ranked || 0,
       deaths: data.deaths_ranked || 0,
       KDRatio: divide(data.kills_ranked, data.deaths_ranked),
-      WLRatio: divide(data.wins_ranked, data.losses_ranked)
+      WLRatio: divide(data.wins_ranked, data.losses_ranked),
+      ratings: getRankedPositions(data)
     };
     /**
      * Mega Skywars Stats
@@ -307,6 +308,21 @@ class SkyWars {
  * @property {number} WLRatio Win Loss ratio
  */
 /**
+ * @typedef {Object} SkywarsRankedStats
+ * @extends SkywarsTotalModeStats
+ * @property {Map<PseudoDate,SkywarsRankData>} ratings Ratings & leaderboard positions
+ */
+/**
+ * @typedef {Object} SkywarsRankData
+ * @property {number} rating Rating of player ( if not found, this is set to 0 )
+ * @property {number} position Position of player ( if not found, this is set to 0 )
+ * @property {Date} date Parsed date
+ */
+/**
+ * @typedef {string} PseudoDate String date, in the format of MM-YY ( YY is 20YY )
+ * @example `10-19` would be October 2019.
+ */
+/**
  * @typedef {Object} SkyWarsModeExtendedStats
  * @property {SkyWarsTotalModeStats} overall Overall Stats
  * @property {SkyWarsModeStats} normal Normal Mode Stats
@@ -335,4 +351,23 @@ function getSkyWarsLevel (xp) {
   if (xp >= 15000) return (xp - 15000) / 10000 + 12;
   const level = totalXp.findIndex((x) => x * 10 - xp > 0);
   return level + ( xp - (totalXp[level - 1] * 10 || 0)) / (totalXp[level] - totalXp[level - 1]) / 10;
+}
+const ratingRegex = /^SkyWars_skywars_rating_(\d{1,2})_(\d{1,2})_(position|rating)$/;
+/**
+ * gets ratings & positions on the leaderboard
+ * @param {Object} data data
+ * @returns {SkywarsRankedStats} some map
+ */
+function getRankedPositions(data) {
+  const map = new Map();
+  const keys = Object.keys(data.stats.Skywars).map((key) => key.match(ratingRegex)).filter((x)=>x);
+  for (const key of keys) {
+    const computedKey = `${(key[1].length - 1 ? '0':'')}${key[1]}-${key[2]}`;
+    map.set(computedKey, {
+      ...map.get(computedKey),
+      [key[3]]: parseInt(data.stats.Skywars[key[0]], 10) || 0,
+      date: new Date(new Date().setFullYear(2000 + parseInt(key[2], 10), parseInt(key[1], 10) - 1, 1))
+    });
+  }
+  return map;
 }
