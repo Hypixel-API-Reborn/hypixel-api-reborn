a4edca7a5b3ac5e966baf2a2e115a0e602edb5c6
StavZDev <randy10220@gmail.com>
2021-03-09 18:41:17 +0500

    Update getOnline.js

--- src/API/getOnline.js
+++ src/API/getOnline.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch');
 module.exports = async function (server = 'mc.hypixel.net') {
   const res = await fetch(`https://api.mcsrvstat.us/2/${server}`).then((r) => r.json());
-  return res.players.online; // TODO - use this to full potential
+  return res.players?.online ?? 0; // TODO - use this to full potential
 };
