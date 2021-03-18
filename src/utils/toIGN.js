2aa42c9dc182814665c81495ac0c342dc9b24c8b
muchnameless <muchnameless@gmail.com>
2021-03-18 16:45:35 +0100

    toIGN: throw specific errors

--- src/utils/toIGN.js
+++ src/utils/toIGN.js
@@ -3,15 +3,15 @@ const isUUID = require('./isUUID.js');
 const Errors = require('../Errors');
 
 module.exports = async (input) => {
+  if (!input) throw new Error(Errors.NO_NICKNAME_UUID);
+  if (typeof input !== 'string') throw new Error(Errors.UUID_NICKNAME_MUST_BE_A_STRING);
+  if (isUUID(input)) input = input.replace(/-/g, '');
+  else throw new Error(Errors.MALFORMED_UUID);
   try {
-    if (!input) throw new Error(Errors.NO_NICKNAME_UUID);
-    if (typeof input !== 'string') throw new Error(Errors.UUID_NICKNAME_MUST_BE_A_STRING);
-    if (isUUID(input)) input = input.replace(/-/g, '');
-    else throw new Error(Errors.MALFORMED_UUID);
     const res = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${input}`, input);
     const parsedRes = await res.json();
     if (parsedRes.error) {
-      throw new Error(Errors.MALFORMED_UUID);
+      return Promise.reject(new Error(Errors.MALFORMED_UUID));
     }
     return parsedRes.name;
   } catch (e) {
