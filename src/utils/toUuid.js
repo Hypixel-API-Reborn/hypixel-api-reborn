e51928620dc94fd966adc2d6d0be7a39b74edcc1
muchnameless <muchnameless@gmail.com>
2021-03-18 16:45:45 +0100

    toUuid: throw specific errors

--- src/utils/toUuid.js
+++ src/utils/toUuid.js
@@ -3,14 +3,14 @@ const isUUID = require('./isUUID.js');
 const Errors = require('../Errors');
 
 module.exports = async (input) => {
+  if (!input) throw new Error(Errors.NO_NICKNAME_UUID);
+  if (typeof input !== 'string') throw new Error(Errors.UUID_NICKNAME_MUST_BE_A_STRING);
+  if (isUUID(input)) return input.replace(/-/g, '');
   try {
-    if (!input) throw new Error(Errors.NO_NICKNAME_UUID);
-    if (typeof input !== 'string') throw new Error(Errors.UUID_NICKNAME_MUST_BE_A_STRING);
-    if (isUUID(input)) return input.replace(/-/g, '');
     const res = await fetch(`https://api.mojang.com/users/profiles/minecraft/${input}`, input);
     const parsedRes = await res.json();
     if (parsedRes.error) {
-      throw new Error(Errors.MALFORMED_UUID);
+      return Promise.reject(new Error(Errors.MALFORMED_UUID));
     }
     return parsedRes.id;
   } catch (e) {
