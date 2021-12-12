c61c5f8bcde003f76e53e92f38e69508a5a8d9af
StavZDev <randy10220@gmail.com>
2021-12-12 14:45:05 +0500

    dumb

--- src/API/skyblock/getSkyblockAuctions.js
+++ src/API/skyblock/getSkyblockAuctions.js
@@ -5,7 +5,6 @@ const Errors = require('../../Errors');
 let _makeRequest;
 async function getPage (page = 0, options = {}) {
   const content = await _makeRequest(`/skyblock/auctions?page=${page}`, false);
-  if (res.raw) return res;
   const result = {};
   if (!options.noInfo) result.info = new AuctionInfo(content);
   if (options.raw) result.auctions = content.auctions;
