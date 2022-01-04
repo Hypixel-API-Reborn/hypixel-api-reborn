8c70a19a57a161096dc6c2b3b07d75cead96d7bd
RichardDorian <richard.dorian@outlook.com>
2022-01-04 21:15:28 +0100

    Changed files imports

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,13 @@
-/* eslint-disable no-extend-native */
-const fs = require('fs');
-// eslint-disable-next-line no-path-concat
-const curDir = __dirname + '/';
-module.exports = Array.from(fs.readdirSync(curDir, { withFileTypes: true }))
-  .filter((x) => x.name !== 'index.js')
-  .map((x) => x.isDirectory() ? Array.from(fs.readdirSync(curDir + x.name)).map((y) => ([y.split('.')[0], x.name + '/' + y])) : [[x.name.split('.')[0], x.name]])
-  .flat(1)
-  .reduce((pV, cV) => Object.assign(pV, { [cV[0]]: require(curDir + cV[1]) }), {});
+module.exports = {
+  arrayTool: require('./arrayTools'),
+  Constants: require('./Constants'),
+  divide: require('./divide'),
+  isGuildID: require('./isGuildID'),
+  isUUID: require('./isUUID'),
+  oscillation: require('./oscillation'),
+  removeSnakeCase: require('./removeSnakeCase'),
+  SkyblockUtils: require('./SkyblockUtils'),
+  toIGN: require('./toIGN'),
+  toUuid: require('./toUuid'),
+  varInt: require('./varInt')
+};
