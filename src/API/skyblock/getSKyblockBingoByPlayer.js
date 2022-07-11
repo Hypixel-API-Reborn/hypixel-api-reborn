ab5cd3113734d57797af8d20e6fee8f9b1c50b0f
stavzdev <randy10220@gmail.com>
2022-07-11 17:36:31 +0500

    Update getSKyblockBingoByPlayer.js

--- src/API/skyblock/getSKyblockBingoByPlayer.js
+++ src/API/skyblock/getSKyblockBingoByPlayer.js
@@ -5,10 +5,10 @@ module.exports = async function (query, { fetchBingoData = false }) {
   if (!query) throw new Error(Errors.NO_NICKNAME_UUID);
   const PlayerBingo = require('../../structures/SkyBlock/PlayerBingo');
   query = await toUuid(query);
-  const res = await this._makeRequest(`/skyblock/uuid?player=${query}`);
+  const res = await this._makeRequest(`/skyblock/bingo?player=${query}`);
   if (res.raw) return res;
   let bingoData = null;
   if (fetchBingoData) bingoData = await getSkyblockBingo.call(this);
 
-  return new PlayerBingo(data, bingoData);
+  return new PlayerBingo(res, bingoData);
 };
