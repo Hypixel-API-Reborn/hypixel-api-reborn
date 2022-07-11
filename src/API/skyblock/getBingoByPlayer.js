a69647e8cedd5cdf2211eac0f76ee9317f2b2453
ChiefChippy2 <chiefchippy2@gmail.com>
2022-07-11 19:42:49 +0200

    fix(bingo+government): I am a mayor (jk)

--- src/API/skyblock/getBingoByPlayer.js
+++ src/API/skyblock/getBingoByPlayer.js
@@ -1,11 +1,11 @@
 const Errors = require('../../Errors');
 const toUuid = require('../../utils/toUuid');
 const getSkyblockBingo = require('./getBingo');
-module.exports = async function (query, { fetchBingoData = false }) {
+module.exports = async function (query, { fetchBingoData = false } = {}) {
   if (!query) throw new Error(Errors.NO_NICKNAME_UUID);
   const PlayerBingo = require('../../structures/SkyBlock/PlayerBingo');
   query = await toUuid(query);
-  const res = await this._makeRequest(`/skyblock/bingo?player=${query}`);
+  const res = await this._makeRequest(`/skyblock/bingo?uuid=${query}`);
   if (res.raw) return res;
   let bingoData = null;
   if (fetchBingoData) bingoData = await getSkyblockBingo.call(this);
