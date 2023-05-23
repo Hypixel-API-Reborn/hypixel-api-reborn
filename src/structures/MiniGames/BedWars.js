e1ce5ed357d6cdfdaf4073df48a9e784cb6d6b4b
Kath <55346310+Kathund@users.noreply.github.com>
2023-05-23 09:53:31 +0800

    FEAT: Added prestiges 3k-5k

--- src/structures/MiniGames/BedWars.js
+++ src/structures/MiniGames/BedWars.js
@@ -204,7 +204,7 @@ class BedWars {
  */
 function getBedWarsPrestige (level) {
   // eslint-disable-next-line max-len
-  return ['Stone', 'Iron', 'Gold', 'Diamond', 'Emerald', 'Sapphire', 'Ruby', 'Crystal', 'Opal', 'Amethyst', 'Rainbow', 'Iron Prime', 'Gold Prime', 'Diamond Prime', 'Emerald Prime', 'Sapphire Prime', 'Ruby Prime', 'Crystal Prime', 'Opal Prime', 'Amethyst Prime', 'Mirror', 'Light', 'Dawn', 'Dusk', 'Air', 'Wind', 'Nebula', 'Thunder', 'Earth', 'Water', 'Fire'][Math.floor(level / 100)] || 'Rainbow';
+  return ['Stone', 'Iron', 'Gold', 'Diamond', 'Emerald', 'Sapphire', 'Ruby', 'Crystal', 'Opal', 'Amethyst', 'Rainbow', 'Iron Prime', 'Gold Prime', 'Diamond Prime', 'Emerald Prime', 'Sapphire Prime', 'Ruby Prime', 'Crystal Prime', 'Opal Prime', 'Amethyst Prime', 'Mirror', 'Light', 'Dawn', 'Dusk', 'Air', 'Wind', 'Nebula', 'Thunder', 'Earth', 'Water', 'Fire', 'Sunrise', 'Eclipse', 'Gamma', 'Majestic', 'Andesine', 'Marine', 'Element', 'Galaxy', 'Atomic', 'Sunset', 'Time', 'Winter', 'Obsidian', 'Spring', 'Ice', 'Summer', 'Spinel', 'Autumn', 'Mystic', 'Eternal'][Math.floor(level / 100)] || 'Rainbow';
 }
 const EASY_LEVELS = 4;
 const EASY_LEVELS_XP = 7000;
@@ -295,6 +295,26 @@ function getLevelForExp (exp) {
  * * `Earth`
  * * `Water`
  * * `Fire`
+ * * `Sunrise`
+ * * `Eclipse`
+ * * `Gamma`
+ * * `Majestic`
+ * * `Andesine`
+ * * `Marine`
+ * * `Element`
+ * * `Galaxy`
+ * * `Atomic`
+ * * `Sunset`
+ * * `Time`
+ * * `Winter`
+ * * `Obsidian`
+ * * `Spring`
+ * * `Ice`
+ * * `Summer`
+ * * `Spinel`
+ * * `Autumn`
+ * * `Mystic`
+ * * `Eternal`
  */
 /**
  * @typedef {object} BedWarsAvg
