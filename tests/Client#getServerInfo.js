bce94d31e2796dfb7eee497f71388f835507d9a3
Kath <55346310+Kathund@users.noreply.github.com>
2023-10-01 20:08:52 +0800

    Fix dupe test

--- tests/Client#getServerInfo.js
+++ tests/Client#getServerInfo.js
@@ -180,40 +180,5 @@ describe('Client#getServerInfo', async () => {
         }
       });
     });
-    server;
-    describe('No Input test', async () => {
-      it('expect not to throw', async () => {
-        server = await client.getServerInfo('2');
-      });
-      it('should be an object', () => {
-        expect(server).to.be.an('object');
-      });
-      it('required keys should exist', () => {
-        expect(server.protocolUsed).to.be.a('number');
-        expect(server.versionInfo).to.be.a('string');
-        expect(server.players).to.be.an('object');
-        expect(server.players.max).to.be.a('number');
-        expect(server.players.online).to.be.a('number');
-        expect(server.players.players).to.be.an('array');
-        if (server.rawMOTD) {
-          expect(server.rawMOTD).to.be.a('string');
-        }
-        if (server.cleanMOTD) {
-          expect(server.cleanMOTD).to.be.a('string');
-        }
-        if (server.textMOTD) {
-          expect(server.textMOTD).to.be.a('string');
-        }
-        if (server.faviconB64) {
-          expect(server.faviconB64).to.be.a('string');
-        }
-        if (server.favicon) {
-          expect(server.favicon).to.be.instanceOf(Buffer);
-        }
-        if (server.ping) {
-          expect(server.ping).to.be.a('number');
-        }
-      });
-    });
   });
 });
