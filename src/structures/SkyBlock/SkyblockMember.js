feb5fdfad771170b1005ef7957e328c2062e76a2
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2023-12-07 20:33:33 +0100

    Fix updates

--- src/structures/SkyBlock/SkyblockMember.js
+++ src/structures/SkyBlock/SkyblockMember.js
@@ -380,11 +380,11 @@ function getDungeons(data) {
       }
     },
     classes: {
-      healer: getLevelByXp(data.dungeons.player_classes?.healer.experience ?? null, 'dungeons'),
-      mage: getLevelByXp(data.dungeons.player_classes?.mage.experience ?? null, 'dungeons'),
-      berserk: getLevelByXp(data.dungeons.player_classes?.berserk.experience ?? null, 'dungeons'),
-      archer: getLevelByXp(data.dungeons.player_classes?.archer.experience ?? null, 'dungeons'),
-      tank: getLevelByXp(data.dungeons.player_classes?.tank.experience ?? null, 'dungeons')
+      healer: getLevelByXp(data.dungeons.player_classes?.healer?.experience ?? null, 'dungeons'),
+      mage: getLevelByXp(data.dungeons.player_classes?.mage?.experience ?? null, 'dungeons'),
+      berserk: getLevelByXp(data.dungeons.player_classes?.berserk?.experience ?? null, 'dungeons'),
+      archer: getLevelByXp(data.dungeons.player_classes?.archer?.experience ?? null, 'dungeons'),
+      tank: getLevelByXp(data.dungeons.player_classes?.tank?.experience ?? null, 'dungeons')
     }
   };
 }
