3db2f9e7488468f8f66c6e40fd22647a4120fcf8
Kath <admin@kath.lol>
2024-03-06 12:50:44 +0800

    fix uuid cache

--- src/Private/uuidCache.js
+++ src/Private/uuidCache.js
@@ -2,43 +2,22 @@
 const requireFetch = !globalThis.fetch;
 const externalFetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const fetch = requireFetch ? externalFetch : globalThis.fetch;
-const cachedUuids = new Map();
+const NodeCache = require('node-cache');
+const cache = new NodeCache();
 
 // TODO - use this for all cache models
 
-class UUIDCache {
-  constructor(client) {
-    this.client = client;
-  }
-  async putCache(response) {
-    try {
-      response = await response.json();
-      cachedUuids.set(response.id, response.name);
-      setTimeout(deleteCache, this.client.options.mojangCacheTime * 1000, response.id);
-    } catch (e) {
-      // F
-    }
-  }
-  deleteCache(id) {
-    return cachedUuids.delete(id);
-  }
-  response(obj) {
-    return new fetch.Response(JSON.stringify(obj));
-  }
-  checkHit(query) {
-    if (cachedUuids.has(query)) return response({ id: query, name: cachedUuids.get(query) });
-    const reverseSearch = Array.from(cachedUuids.entries()).find((pair) => pair[1].toLowerCase() === query.toLowerCase());
-    if (reverseSearch) return response({ id: reverseSearch[0], name: query });
-    return null;
-  }
-}
-
 module.exports = async (url, query) => {
-  const cache = new UUIDCache();
-  if (cache.checkHit(query || url)) return cache.checkHit(query || url);
+  if (cache.has(query.toLowerCase())) return cache.get(query.toLowerCase());
   const res = await fetch(url);
-  const response = await res.clone().json();
-  // Don't cache 5xx
-  if (response.error !== 'Internal Server Error' && response.status < 500) setTimeout(cache.putCache, 0, res.clone());
-  return res;
+  const data = await res.json();
+  // Don't cache 4xx
+  if (res.status < 400) {
+    cache.set(query.toLowerCase(), { id: data.id, name: data.name }, 60 * 60 * 24);
+    cache.set(data.id.toLowerCase(), { id: data.id, name: data.name }, 60 * 60 * 24);
+  }
+  return {
+    id: data.id,
+    name: data.name
+  };
 };
