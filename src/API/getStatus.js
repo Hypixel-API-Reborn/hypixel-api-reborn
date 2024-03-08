44d99152ede246d2cc7e72ebcca518f7600e7f5c
Kath <55346310+Kathund@users.noreply.github.com>
2024-03-08 22:10:02 +0800

    fix(uuidCache) (#505)
    * fix uuid cache
    
    * fix bad response
    
    * fix invalid players
    
    * Cache Times
    
    * Remove testing code
    
    * Refactor cacheTime parameter to use global instead
    
    * revert use global for cacheTime
    
    * Update src/Client.js
    
    ---------
    
    Co-authored-by: ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>

--- src/API/getStatus.js
+++ src/API/getStatus.js
@@ -1,7 +1,7 @@
 const toUuid = require('../utils/toUuid');
 module.exports = async function (query) {
   const Status = require('../structures/Status');
-  query = await toUuid(query);
+  query = await toUuid(query, this.options.mojangCacheTime);
   const res = await this._makeRequest(`/status?uuid=${query}`);
   if (res.raw) return res;
   return new Status(res.session);
