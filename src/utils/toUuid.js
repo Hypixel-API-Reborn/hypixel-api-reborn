c759b4c09945e5e6a202fc0c3464c85878a91eb6
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2024-04-06 12:32:06 +0200

    ffs mojang api

--- src/utils/toUuid.js
+++ src/utils/toUuid.js
@@ -2,12 +2,13 @@ const fetch = require('../Private/uuidCache.js');
 const isUUID = require('./isUUID.js');
 const Errors = require('../Errors');
 
-module.exports = async (input, cacheTime = 600) => {
+module.exports = async (input, cacheTime = 600, useThirdPartyAPI = false) => {
   if (!input) throw new Error(Errors.NO_NICKNAME_UUID);
   if (typeof input !== 'string') throw new Error(Errors.UUID_NICKNAME_MUST_BE_A_STRING);
   if (isUUID(input)) return input.replace(/-/g, '');
   try {
-    const res = await fetch(`https://api.mojang.com/users/profiles/minecraft/${input}`, input, cacheTime);
+    const url = useThirdPartyAPI ? `https://api.minetools.eu/uuid/${input}` : `https://api.mojang.com/users/profiles/minecraft/${input}`;
+    const res = await fetch(url, input, cacheTime);
     if (res.status === 404) {
       return Promise.reject(new Error(Errors.PLAYER_DOES_NOT_EXIST));
     } else if (res.status !== 200) {
@@ -15,6 +16,8 @@ module.exports = async (input, cacheTime = 600) => {
     }
     return res.id;
   } catch {
+    // 2nd Try
+    if (!useThirdPartyAPI) return this(input, cacheTime, true);
     throw new Error(Errors.PLAYER_DOES_NOT_EXIST);
   }
 };
