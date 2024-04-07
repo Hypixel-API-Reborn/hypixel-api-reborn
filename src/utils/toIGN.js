14675984e6975d0c6a1c5f5f7d1ac44bd3f25c5f
ChiefChippy2 <45596322+ChiefChippy2@users.noreply.github.com>
2024-04-07 21:28:45 +0200

    custom url

--- src/utils/toIGN.js
+++ src/utils/toIGN.js
@@ -2,12 +2,14 @@ const fetch = require('../Private/uuidCache.js');
 const isUUID = require('./isUUID.js');
 const Errors = require('../Errors');
 
-module.exports = async (input) => {
+module.exports = async (input, useThirdPartyAPI = "") => {
   if (!input) throw new Error(Errors.NO_NICKNAME_UUID);
   if (typeof input !== 'string') throw new Error(Errors.UUID_NICKNAME_MUST_BE_A_STRING);
   if (isUUID(input)) input = input.replace(/-/g, '');
   else throw new Error(Errors.MALFORMED_UUID);
   try {
+    const customUrl = useThirdPartyAPI === true ? "https://api.minetools.eu/uuid/" : useThirdPartyAPI;
+    const url = useThirdPartyAPI ? `${customUrl}${input}` : `https://api.mojang.com/users/profiles/minecraft/${input}`;
     const res = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${input}`, input);
     const parsedRes = await res.json();
     if (parsedRes.error) {
@@ -15,6 +17,8 @@ module.exports = async (input) => {
     }
     return parsedRes.name;
   } catch {
+    // 2nd try
+    if (!useThirdPartyAPI) return this(input, true);
     throw new Error(Errors.PLAYER_DOES_NOT_EXIST);
   }
 };
--- src/utils/toUuid.js
+++ src/utils/toUuid.js
@@ -2,12 +2,13 @@ const fetch = require('../Private/uuidCache.js');
 const isUUID = require('./isUUID.js');
 const Errors = require('../Errors');
 
-module.exports = async (input, cacheTime = 600, useThirdPartyAPI = false) => {
+module.exports = async (input, cacheTime = 600, useThirdPartyAPI = "") => {
   if (!input) throw new Error(Errors.NO_NICKNAME_UUID);
   if (typeof input !== 'string') throw new Error(Errors.UUID_NICKNAME_MUST_BE_A_STRING);
   if (isUUID(input)) return input.replace(/-/g, '');
   try {
-    const url = useThirdPartyAPI ? `https://api.minetools.eu/uuid/${input}` : `https://api.mojang.com/users/profiles/minecraft/${input}`;
+    const customUrl = useThirdPartyAPI === true ? "https://api.minetools.eu/uuid/" : useThirdPartyAPI;
+    const url = useThirdPartyAPI ? `${customUrl}${input}` : `https://api.mojang.com/users/profiles/minecraft/${input}`;
     const res = await fetch(url, input, cacheTime);
     if (res.status === 404) {
       return Promise.reject(new Error(Errors.PLAYER_DOES_NOT_EXIST));
