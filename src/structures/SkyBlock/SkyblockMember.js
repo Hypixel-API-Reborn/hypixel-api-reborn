b6e29f8a496d38dc80696e40b6d1a9d1b127b502
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2024-08-05 14:29:49 +0000

    add kuudra

--- src/structures/SkyBlock/SkyblockMember.js
+++ src/structures/SkyBlock/SkyblockMember.js
@@ -129,6 +129,11 @@ class SkyblockMember {
      */
     this.slayer = getSlayer(data.m);
     /**
+     * Skyblock member kuudra
+     * @type {NetherIslandPlayerData|null}
+     */
+    this.kuudra = data.m.nether_island_player_data?.kuudra_completed_tiers;
+    /**
      * Skyblock member dungeons
      * @type {SkyblockMemberDungeons|null}
      */
@@ -393,6 +398,18 @@ class SkyblockMember {
  * @property {number} tier4 Tier 4
  * @property {number} tier5 Tier 5
  * @property {number} level Level
+ */
+/**
+ * @typedef {object} NetherIslandPlayerData
+ * @property {number} none
+ * @property {number} hot
+ * @property {number} burning
+ * @property {number} fiery
+ * @property {number} highest_wave_hot
+ * @property {number} highest_wave_fiery
+ * @property {number} infernal
+ * @property {number} highest_wave_infernal
+ * @property {number} highest_wave_burning
  */
 /**
  * @typedef {object} SkyblockMemberDungeons
