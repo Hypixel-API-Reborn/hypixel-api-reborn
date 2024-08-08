ef937fd1a6aaca569cac27908c12d21ab6cd7b05
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2024-08-08 23:25:16 +0200

    Revert changes

--- src/API/skyblock/getGarden.js
+++ src/API/skyblock/getGarden.js
@@ -1,10 +1,7 @@
 module.exports = async function (profileId) {
   const SkyblockGarden = require('../../structures/SkyBlock/SkyblockGarden');
-  let res;
   // eslint-disable-next-line no-underscore-dangle
-  try {
-      res = await this._makeRequest(`/skyblock/garden?profile=${profileId}`);
-  } catch(e) {}
-  if (res?.raw) return res;
+  const res = await this._makeRequest(`/skyblock/garden?profile=${profileId}`);
+  if (res.raw) return res;
   return new SkyblockGarden(res);
 };
