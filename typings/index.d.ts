6a84fb2e53945f4275128276b93b7aeb4bcf1d9c
Johannes7k75 <57235955+Johannes7k75@users.noreply.github.com>
2024-09-09 22:30:08 +0200

    Update index.d.ts

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2756,12 +2756,15 @@ declare module 'hypixel-api-reborn' {
         mages: number;
       };
       trophyFish: {
-        total: number;
-        bronze: number;
-        silver: number;
-        gold: number;
-        diamond: number;
-      };
+				rank: "Diamond" | "Gold" | "Silver" | "Bronze" | null;
+				caught: {
+					total: number;
+					bronze: number;
+					silver: number;
+					gold: number;
+					diamond: number;
+				};
+			};
       kuudra: {
         none: number;
         hot: number;
