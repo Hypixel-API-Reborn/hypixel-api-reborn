025c539e1f3d5895641fd38bc755095702f0e2cc
Johannes7k75 <Jonashebenstreit1@gmail.com>
2024-11-22 16:46:27 +0100

    fix pet score

--- src/structures/SkyBlock/SkyblockMember.js
+++ src/structures/SkyBlock/SkyblockMember.js
@@ -244,6 +244,9 @@ class SkyblockMember {
       for (const pet of data.m.pets_data.pets) {
         if (!(pet.type in highestRarity) || Constants.petScore[pet.tier] > highestRarity[pet.type]) {
           highestRarity[pet.type] = Constants.petScore[pet.tier];
+          if (pet.heldItem === 'PET_ITEM_TIER_BOOST' && pet.tier !== 'MYTHIC') {
+            highestRarity[pet.type] += 1;
+          }
         }
       }
 
