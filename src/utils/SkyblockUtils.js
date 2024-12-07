3bfe940462acbfc585f42a99013325052357a582
Yoshy <60155833+Yoshy2002@users.noreply.github.com>
2024-12-07 07:46:24 +0100

    Fix/Update Bestiary + Fix Trophy_Fish rewards.length = 5 (#637)
    * Fix/Update Bestiary
    
    * Fix Trophy Fishing
    
    * Change to <= 4

--- src/utils/SkyblockUtils.js
+++ src/utils/SkyblockUtils.js
@@ -177,7 +177,7 @@ function getTrophyFishRank(level) {
     return 'Silver';
   } else if (3 === level) {
     return 'Gold';
-  } else if (4 === level) {
+  } else if (4 <= level) {
     return 'Diamond';
   }
   return null;
