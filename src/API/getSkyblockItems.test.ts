10d4ae097bc0417226ec6a0003c7aa3690c11eb8
Jacob <admin@kath.lol>
2025-03-10 21:40:14 +0800

    fix eslint & prettier

--- src/API/getSkyblockItems.test.ts
+++ src/API/getSkyblockItems.test.ts
@@ -22,29 +22,29 @@ test('getSkyblockItems', async () => {
     expect(item.durability).toBeDefined();
     expectTypeOf(item.durability).toEqualTypeOf<number | undefined>();
 
-    expect(item.museum_data).toBeDefined();
-    expectTypeOf(item.museum_data).toEqualTypeOf<Record<string, any> | undefined>();
+    expect(item.museumData).toBeDefined();
+    expectTypeOf(item.museumData).toEqualTypeOf<Record<string, any> | undefined>();
 
-    expect(item.has_uuid).toBeDefined();
-    expectTypeOf(item.has_uuid).toEqualTypeOf<boolean | string | undefined>();
+    expect(item.hasUuid).toBeDefined();
+    expectTypeOf(item.hasUuid).toEqualTypeOf<boolean | string | undefined>();
 
-    expect(item.gemstone_slots).toBeDefined();
-    expectTypeOf(item.gemstone_slots).toEqualTypeOf<Record<string, any>[] | undefined>();
+    expect(item.gemstoneSlots).toBeDefined();
+    expectTypeOf(item.gemstoneSlots).toEqualTypeOf<Record<string, any>[] | undefined>();
 
     expect(item.glowing).toBeDefined();
     expectTypeOf(item.glowing).toEqualTypeOf<boolean | undefined>();
 
-    expect(item.can_auction).toBeDefined();
-    expectTypeOf(item.can_auction).toEqualTypeOf<boolean | undefined>();
+    expect(item.canAuction).toBeDefined();
+    expectTypeOf(item.canAuction).toEqualTypeOf<boolean | undefined>();
 
-    expect(item.can_trade).toBeDefined();
-    expectTypeOf(item.can_trade).toEqualTypeOf<boolean | undefined>();
+    expect(item.canTrade).toBeDefined();
+    expectTypeOf(item.canTrade).toEqualTypeOf<boolean | undefined>();
 
     expect(item.requirements).toBeDefined();
     expectTypeOf(item.requirements).toEqualTypeOf<Record<string, any>[] | undefined>();
 
-    expect(item.can_place).toBeDefined();
-    expectTypeOf(item.can_place).toEqualTypeOf<boolean | undefined>();
+    expect(item.canPlace).toBeDefined();
+    expectTypeOf(item.canPlace).toEqualTypeOf<boolean | undefined>();
 
     expect(item.museum).toBeDefined();
     expectTypeOf(item.museum).toEqualTypeOf<boolean | undefined>();
@@ -52,74 +52,74 @@ test('getSkyblockItems', async () => {
     expect(item.generator).toBeDefined();
     expectTypeOf(item.generator).toEqualTypeOf<string | undefined>();
 
-    expect(item.generator_tier).toBeDefined();
-    expectTypeOf(item.generator_tier).toEqualTypeOf<number | undefined>();
+    expect(item.generatorTier).toBeDefined();
+    expectTypeOf(item.generatorTier).toEqualTypeOf<number | undefined>();
 
     expect(item.furniture).toBeDefined();
     expectTypeOf(item.furniture).toEqualTypeOf<string | undefined>();
 
-    expect(item.item_specific).toBeDefined();
-    expectTypeOf(item.item_specific).toEqualTypeOf<Record<string, any> | undefined>();
+    expect(item.itemSpecific).toBeDefined();
+    expectTypeOf(item.itemSpecific).toEqualTypeOf<Record<string, any> | undefined>();
 
     expect(item.description).toBeDefined();
     expectTypeOf(item.description).toEqualTypeOf<string | undefined>();
 
-    expect(item.upgrade_costs).toBeDefined();
-    expectTypeOf(item.upgrade_costs).toEqualTypeOf<Array<Array<Record<string, any>>> | undefined>();
+    expect(item.upgradeCosts).toBeDefined();
+    expectTypeOf(item.upgradeCosts).toEqualTypeOf<Array<Array<Record<string, any>>> | undefined>();
 
-    expect(item.gear_score).toBeDefined();
-    expectTypeOf(item.gear_score).toEqualTypeOf<number | undefined>();
+    expect(item.gearScore).toBeDefined();
+    expectTypeOf(item.gearScore).toEqualTypeOf<number | undefined>();
 
-    expect(item.dungeon_item).toBeDefined();
-    expectTypeOf(item.dungeon_item).toEqualTypeOf<boolean | undefined>();
+    expect(item.dungeonItem).toBeDefined();
+    expectTypeOf(item.dungeonItem).toEqualTypeOf<boolean | undefined>();
 
-    expect(item.dungeon_item_conversion_cost).toBeDefined();
-    expectTypeOf(item.dungeon_item_conversion_cost).toEqualTypeOf<Record<string, any> | undefined>();
+    expect(item.dungeonItemConversionCost).toBeDefined();
+    expectTypeOf(item.dungeonItemConversionCost).toEqualTypeOf<Record<string, any> | undefined>();
 
-    expect(item.catacombs_requirements).toBeDefined();
-    expectTypeOf(item.catacombs_requirements).toEqualTypeOf<Record<string, any>[] | undefined>();
+    expect(item.catacombsRequirements).toBeDefined();
+    expectTypeOf(item.catacombsRequirements).toEqualTypeOf<Record<string, any>[] | undefined>();
 
-    expect(item.can_have_attributes).toBeDefined();
-    expectTypeOf(item.can_have_attributes).toEqualTypeOf<boolean | undefined>();
+    expect(item.canHaveAttributes).toBeDefined();
+    expectTypeOf(item.canHaveAttributes).toEqualTypeOf<boolean | undefined>();
 
-    expect(item.salvageable_from_recipe).toBeDefined();
-    expectTypeOf(item.salvageable_from_recipe).toEqualTypeOf<boolean | undefined>();
+    expect(item.salvageableFromRecipe).toBeDefined();
+    expectTypeOf(item.salvageableFromRecipe).toEqualTypeOf<boolean | undefined>();
 
-    expect(item.can_recombobulate).toBeDefined();
-    expectTypeOf(item.can_recombobulate).toEqualTypeOf<boolean | undefined>();
+    expect(item.canRecombobulate).toBeDefined();
+    expectTypeOf(item.canRecombobulate).toEqualTypeOf<boolean | undefined>();
 
     expect(item.enchantments).toBeDefined();
     expectTypeOf(item.enchantments).toEqualTypeOf<Record<string, any> | undefined>();
 
-    expect(item.rift_transferrable).toBeDefined();
-    expectTypeOf(item.rift_transferrable).toEqualTypeOf<boolean | undefined>();
+    expect(item.riftTransferrable).toBeDefined();
+    expectTypeOf(item.riftTransferrable).toEqualTypeOf<boolean | undefined>();
 
     expect(item.origin).toBeDefined();
     expectTypeOf(item.origin).toEqualTypeOf<string | undefined>();
 
-    expect(item.double_tap_to_drop).toBeDefined();
-    expectTypeOf(item.double_tap_to_drop).toEqualTypeOf<boolean | undefined>();
+    expect(item.doubleTapToDrop).toBeDefined();
+    expectTypeOf(item.doubleTapToDrop).toEqualTypeOf<boolean | undefined>();
 
-    expect(item.hide_from_viewrecipe_command).toBeDefined();
-    expectTypeOf(item.hide_from_viewrecipe_command).toEqualTypeOf<boolean | undefined>();
+    expect(item.hideFromViewRecipeCommand).toBeDefined();
+    expectTypeOf(item.hideFromViewRecipeCommand).toEqualTypeOf<boolean | undefined>();
 
-    expect(item.sword_type).toBeDefined();
-    expectTypeOf(item.sword_type).toEqualTypeOf<string | undefined>();
+    expect(item.swordType).toBeDefined();
+    expectTypeOf(item.swordType).toEqualTypeOf<string | undefined>();
 
-    expect(item.ability_damage_scaling).toBeDefined();
-    expectTypeOf(item.ability_damage_scaling).toEqualTypeOf<number | undefined>();
+    expect(item.abilityDamageScaling).toBeDefined();
+    expectTypeOf(item.abilityDamageScaling).toEqualTypeOf<number | undefined>();
 
-    expect(item.tiered_stats).toBeDefined();
-    expectTypeOf(item.tiered_stats).toEqualTypeOf<Record<string, any> | undefined>();
+    expect(item.tieredStats).toBeDefined();
+    expectTypeOf(item.tieredStats).toEqualTypeOf<Record<string, any> | undefined>();
 
-    expect(item.motes_sell_price).toBeDefined();
-    expectTypeOf(item.motes_sell_price).toEqualTypeOf<number | undefined>();
+    expect(item.motesSellPrice).toBeDefined();
+    expectTypeOf(item.motesSellPrice).toEqualTypeOf<number | undefined>();
 
     expect(item.crystal).toBeDefined();
     expectTypeOf(item.crystal).toEqualTypeOf<string | undefined>();
 
-    expect(item.can_burn_in_furnace).toBeDefined();
-    expectTypeOf(item.can_burn_in_furnace).toEqualTypeOf<boolean | undefined>();
+    expect(item.canBurnInFurnace).toBeDefined();
+    expectTypeOf(item.canBurnInFurnace).toEqualTypeOf<boolean | undefined>();
 
     expect(item.salvage).toBeDefined();
     expectTypeOf(item.salvage).toEqualTypeOf<Record<string, any> | undefined>();
@@ -127,32 +127,32 @@ test('getSkyblockItems', async () => {
     expect(item.serializable).toBeDefined();
     expectTypeOf(item.serializable).toEqualTypeOf<boolean | undefined>();
 
-    expect(item.can_interact).toBeDefined();
-    expectTypeOf(item.can_interact).toEqualTypeOf<boolean | undefined>();
+    expect(item.canInteract).toBeDefined();
+    expectTypeOf(item.canInteract).toEqualTypeOf<boolean | undefined>();
 
-    expect(item.can_interact_right_click).toBeDefined();
-    expectTypeOf(item.can_interact_right_click).toEqualTypeOf<boolean | undefined>();
+    expect(item.canInteractRightClick).toBeDefined();
+    expectTypeOf(item.canInteractRightClick).toEqualTypeOf<boolean | undefined>();
 
-    expect(item.private_island).toBeDefined();
-    expectTypeOf(item.private_island).toEqualTypeOf<string | undefined>();
+    expect(item.privateIsland).toBeDefined();
+    expectTypeOf(item.privateIsland).toEqualTypeOf<string | undefined>();
 
-    expect(item.can_have_power_scroll).toBeDefined();
-    expectTypeOf(item.can_have_power_scroll).toEqualTypeOf<boolean | undefined>();
+    expect(item.canHavePowerScroll).toBeDefined();
+    expectTypeOf(item.canHavePowerScroll).toEqualTypeOf<boolean | undefined>();
 
-    expect(item.can_interact_entity).toBeDefined();
-    expectTypeOf(item.can_interact_entity).toEqualTypeOf<boolean | undefined>();
+    expect(item.canInteractEntity).toBeDefined();
+    expectTypeOf(item.canInteractEntity).toEqualTypeOf<boolean | undefined>();
 
-    expect(item.MINING_FORTUNE).toBeDefined();
-    expectTypeOf(item.MINING_FORTUNE).toEqualTypeOf<number | undefined>();
+    expect(item.miningFortune).toBeDefined();
+    expectTypeOf(item.miningFortune).toEqualTypeOf<number | undefined>();
 
     expect(item.recipes).toBeDefined();
     expectTypeOf(item.recipes).toEqualTypeOf<Record<string, any>[] | undefined>();
 
-    expect(item.cannot_reforge).toBeDefined();
-    expectTypeOf(item.cannot_reforge).toEqualTypeOf<boolean | undefined>();
+    expect(item.cannotReforge).toBeDefined();
+    expectTypeOf(item.cannotReforge).toEqualTypeOf<boolean | undefined>();
 
-    expect(item.lose_motes_value_on_transfer).toBeDefined();
-    expectTypeOf(item.lose_motes_value_on_transfer).toEqualTypeOf<boolean | undefined>();
+    expect(item.loseMotesValueOnTransfer).toBeDefined();
+    expectTypeOf(item.loseMotesValueOnTransfer).toEqualTypeOf<boolean | undefined>();
 
     expect(item.prestige).toBeDefined();
     expectTypeOf(item.prestige).toEqualTypeOf<Record<string, any> | undefined>();
