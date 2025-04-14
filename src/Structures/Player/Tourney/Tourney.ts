78743a2e71b1f147a428fecfac524665f29c675b
Jacob <admin@kath.lol>
2025-04-14 14:55:29 +0800

    convert `turney` -> `tourney`

--- src/Structures/Player/Tourney/Tourney.ts
+++ src/Structures/Player/Tourney/Tourney.ts
@@ -5,16 +5,16 @@ class Tourney {
   totalTributes: number;
   shopSort: string;
   hidePurchased: boolean;
-  turneyData: TourneyData[];
+  tourneyData: TourneyData[];
   constructor(data: Record<string, any>) {
     this.firstJoinLobby = data.first_join_lobby || null;
     this.totalTributes = data.total_tributes || 0;
     this.shopSort = data.shop_sort || '';
     this.hidePurchased = data.hide_purchased || false;
-    this.turneyData = [];
+    this.tourneyData = [];
     Object.keys(data)
       .filter((key) => ['first_join_lobby', 'total_tributes', 'shop_sort', 'hide_purchased'].includes(key))
-      .forEach((key) => this.turneyData.push(new TourneyData(data[key])));
+      .forEach((key) => this.tourneyData.push(new TourneyData(data[key])));
   }
 }
 
