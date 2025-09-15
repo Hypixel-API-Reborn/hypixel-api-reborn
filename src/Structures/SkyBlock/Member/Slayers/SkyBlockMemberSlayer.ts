bf171159ff54425df6dae2f2f1b03e0a7fc252c9
yoshy2002 <jascha.morzuch@gmail.com>
2025-09-15 19:48:10 +0200

    rename kill tiers

--- src/Structures/SkyBlock/Member/Slayers/SkyBlockMemberSlayer.ts
+++ src/Structures/SkyBlock/Member/Slayers/SkyBlockMemberSlayer.ts
@@ -4,19 +4,19 @@ import type { LevelData, SkyBlockSlayer } from '../../../../Types/SkyBlock.js';
 
 class SkyBlockMemberSlayer {
   claimedLevels: SkyBlockMemberSlayerClaimedLevels;
-  tier0Kills: number;
   tier1Kills: number;
   tier2Kills: number;
   tier3Kills: number;
   tier4Kills: number;
+  tier5Kills: number;
   level: LevelData;
   constructor(data: Record<string, any>, slayer: SkyBlockSlayer) {
     this.claimedLevels = new SkyBlockMemberSlayerClaimedLevels(data?.claimed_levels || {});
-    this.tier0Kills = data?.boss_kills_tier_0 || 0;
-    this.tier1Kills = data?.boss_kills_tier_1 || 0;
-    this.tier2Kills = data?.boss_kills_tier_2 || 0;
-    this.tier3Kills = data?.boss_kills_tier_3 || 0;
-    this.tier4Kills = data?.boss_kills_tier_4 || 0;
+    this.tier1Kills = data?.boss_kills_tier_0 || 0;
+    this.tier2Kills = data?.boss_kills_tier_1 || 0;
+    this.tier3Kills = data?.boss_kills_tier_2 || 0;
+    this.tier4Kills = data?.boss_kills_tier_3 || 0;
+    this.tier5Kills = data?.boss_kills_tier_4 || 0;
     this.level = getSlayerLevel(slayer, data?.xp);
   }
 
