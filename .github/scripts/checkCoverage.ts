2fea995dfac3489cb2cdd5f9a4a1424fb27145cf
Jacob <kathund@proton.me>
2025-11-19 13:18:13 +0800

    chore(github actions): Update coverage checking

--- .github/scripts/checkCoverage.ts
+++ .github/scripts/checkCoverage.ts
@@ -1,56 +1,43 @@
 /* v8 ignore next 1000 */
 
-import { parseString } from 'xml2js';
-import { readFile } from 'fs';
+import { parseStringPromise } from 'xml2js';
+import { readFileSync } from 'fs';
 
-function parseXMLFile(): Promise<any> {
-  return new Promise((resolve, reject) => {
-    readFile('.github/coverageData.xml', 'utf8', (err, data) => {
-      if (err) {
-        reject('File Dose not exist!');
-        return;
-      }
+const coverageDataFile = readFileSync('./coverage/clover.xml', 'utf-8');
+const parsed = await parseStringPromise(coverageDataFile);
 
-      parseString(data, (err, result) => {
-        if (err) {
-          reject('Something Went wrong!');
-          return;
-        }
-
-        resolve(result);
-      });
-    });
-  });
-}
-
-const data = await parseXMLFile();
-const codeStats = data.coverage.project[0].metrics[0].$;
+const codeStats = parsed.coverage.project[0].metrics[0].$;
 const info = {
   statements: Number(codeStats.statements),
-  coveredstatements: Number(codeStats.coveredstatements),
+  coveredStatements: Number(codeStats.coveredstatements),
   conditionals: Number(codeStats.conditionals),
-  coveredconditionals: Number(codeStats.coveredconditionals),
+  coveredConditionals: Number(codeStats.coveredconditionals),
   methods: Number(codeStats.methods),
-  coveredmethods: Number(codeStats.coveredmethods)
+  coveredMethods: Number(codeStats.coveredmethods)
 };
 
-if ((info.coveredstatements / info.statements) * 100 < 95) {
-  throw new Error('Statements is required to be 95% or higher');
+const baseline = 95;
+
+const statementsCoverage = (info.coveredStatements / info.statements) * 100;
+if (statementsCoverage <= baseline) {
+  console.log(`Statements is required to be ${baseline}% or higher. Currently ${statementsCoverage.toFixed(2)}`);
+  process.exit();
 }
 
-if ((info.coveredconditionals / info.conditionals) * 100 < 95) {
-  throw new Error('Conditionals is required to be 95% or higher');
+const conditionalsCoverage = (info.coveredConditionals / info.conditionals) * 100;
+if (conditionalsCoverage <= baseline) {
+  console.log(`Conditionals is required to be ${baseline}% or higher. Currently ${conditionalsCoverage.toFixed(2)}`);
+  process.exit();
 }
 
-if ((info.coveredmethods / info.methods) * 100 < 95) {
-  throw new Error('Methods is required to be 95% or higher');
+const methodsCoverage = (info.coveredMethods / info.methods) * 100;
+if (methodsCoverage <= baseline) {
+  console.log(`Methods is required to be ${baseline}% or higher. Currently ${methodsCoverage.toFixed(2)}`);
+  process.exit();
 }
 
-if (
-  ((info.coveredstatements + info.coveredconditionals + info.coveredmethods) /
-    (info.statements + info.conditionals + info.methods)) *
-    100 <
-  95
-) {
-  throw new Error('Everythng Combinded is required to be 95% or higher');
+const combinedCoverage = statementsCoverage + conditionalsCoverage + methodsCoverage;
+if (combinedCoverage <= baseline) {
+  console.log(`Everything Combined is required to be ${baseline}% or higher. Currently ${combinedCoverage.toFixed(2)}`);
+  process.exit();
 }
