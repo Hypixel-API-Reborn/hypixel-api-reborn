2c072d19c3f38f3f809118a25a832a746f27aa02
Saad Nadeem <saad29nadeem@gmail.com>
2025-11-19 23:20:07 -0500

    feat(CaptureTheWool): more stats

--- src/Structures/MiniGames/WoolGames/CaptureTheWool.ts
+++ src/Structures/MiniGames/WoolGames/CaptureTheWool.ts
@@ -11,6 +11,18 @@ class CaptureTheWool {
   woolCaptured: number;
   woolStolen: number;
   woolCaptureStolenRatio: number;
+  killsOnWoolholder: number;
+  goldSpent: number;
+  goldEarned: number;
+  wins: number;
+  losses: number;
+  WLRatio: number;
+  fastestWoolCapture: number;
+  fastestWin: number;
+  deathsToWoolholder: number;
+  longestGame: number;
+  mostGoldEarned: number;
+  mostKillsAndAssists: number;
   constructor(data: Record<string, any>) {
     this.kills = data?.stats?.kills || 0;
     this.assists = data?.stats?.assists || 0;
@@ -22,6 +34,18 @@ class CaptureTheWool {
     this.woolCaptured = data?.stats?.wools_captured || 0;
     this.woolStolen = data?.stats?.wools_stolen || 0;
     this.woolCaptureStolenRatio = Divide(this.woolCaptured, this.woolStolen);
+    this.killsOnWoolholder = data?.stats?.kills_on_woolholder || 0;
+    this.goldSpent = data?.stats?.gold_spent || 0;
+    this.goldEarned = data?.stats?.gold_earned || 0;
+    this.wins = data?.stats?.participated_wins || 0;
+    this.losses = data?.stats?.participated_losses || 0;
+    this.WLRatio = Divide(this.wins, this.losses);
+    this.fastestWoolCapture = data?.stats?.fastest_wool_capture || 0;
+    this.fastestWin = data?.stats?.fastest_win || 0;
+    this.deathsToWoolholder = data?.stats?.deaths_to_woolholder || 0;
+    this.longestGame = data?.stats?.longest_game || 0;
+    this.mostGoldEarned = data?.stats?.most_gold_earned || 0;
+    this.mostKillsAndAssists = data?.stats?.most_kills_and_assists || 0;
   }
 }
 
