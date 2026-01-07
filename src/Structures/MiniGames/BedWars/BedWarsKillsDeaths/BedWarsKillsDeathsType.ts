37f497e9c902c2f2f61cca0d79ae84858911453a
mic0 <59290502+mic0ishere@users.noreply.github.com>
2026-01-07 19:33:44 +0100

    fix(BedWarsKillsDeathsType): fix kdr calculation for BedWarsKillsDeathsType

--- src/Structures/MiniGames/BedWars/BedWarsKillsDeaths/BedWarsKillsDeathsType.ts
+++ src/Structures/MiniGames/BedWars/BedWarsKillsDeaths/BedWarsKillsDeathsType.ts
@@ -6,9 +6,10 @@ class BedWarsKillsDeathsType extends BaseKillsDeathsType {
   constructor(data: Record<string, any>, type?: BedWarsFinalType, mode?: BedWarsModeId, finals: boolean = false) {
     type = ParseModeBefore(type) as BedWarsFinalType;
     mode = ParseModeBefore(mode) as BedWarsModeId;
-    super(data);
-    this.kills = data?.[`${mode}${type}${finals ? 'final_' : ''}kills_bedwars`] || 0;
-    this.deaths = data?.[`${mode}${type}${finals ? 'final_' : ''}deaths_bedwars`] || 0;
+    super({
+      kills: data?.[`${mode}${type}${finals ? 'final_' : ''}kills_bedwars`] || 0,
+      deaths: data?.[`${mode}${type}${finals ? 'final_' : ''}deaths_bedwars`] || 0
+    });
   }
 }
 
